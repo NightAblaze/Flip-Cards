0e67621dcbd32d0efab11e434b619ef763507fed
NightAblaze <niallw86@googlemail.com>
2023-04-22 11:44:52 +0100

    Bug fixes.

--- scripts.js
+++ scripts.js
@@ -1,42 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let cardArray = {};
+    let cardArray = [];
 
     // create cards
     for (let i = 0; i < 16; i++){
         cardArray[i] = {shape: "triangle", colour: "red"}
     }
-
-    console.log(cardArray) // testing array has populated
-    console.log(cardArray.length); // NEEDS SORTING - CURRENTLY UNDEFINED
-
-    const cardback = document.createElement('text')
+    // console.log(cardArray) // testing array has populated - working
+    
+    //create the back of the cards
+    const cardback = document.createElementNS('http://www.w3.org/2000/svg', 'text')
         cardback.setAttribute('x','25');
         cardback.setAttribute('y','80');
         cardback.setAttribute('class','cardback');
         cardback.setAttribute('stroke','black');
         cardback.setAttribute('stroke-width','3');
         cardback.setAttribute('fill','grey');
-        document.getElementsByClassName("cardback").innerHTML = "?";
+        cardback.innerHTML = "?"
 
-    console.log(cardback); // testing cardback is working
+    // console.log(cardback); // testing cardback is working - working
 
-    const triangle = document.createElement('polygon')
-        triangle.setAttribute('points','50,10 90,90 10,90');
-        triangle.setAttribute('stroke','black');
-        triangle.setAttribute('stroke-width','3');
-        triangle.setAttribute('fill','green');
-
+    // assign grid variable
     const grid = document.querySelector('.grid');
+    
+    // assign triangle variable for testing purposes
+    let triangle = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+    triangle.setAttribute('points','50,10 90,90 10,90');
+    triangle.setAttribute('stroke','black');
+    triangle.setAttribute('stroke-width','3');
+    triangle.setAttribute('fill','green');
+
+
 
     // create the board
     function createBoard() {
-        for (let j = 0; j < 16; j++) {
-            let card = document.createElement('svg');
+        for (let j = 0; j < cardArray.length; j++) {
+            var card = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
             card.setAttribute('data-id', j);
             card.setAttribute('width', '100');
             card.setAttribute('height', '100');
-            card.appendChild(cardback);
-            // card.addEventListener('click', flipcard); // not created yet
+            card.appendChild(cardback); //  ONLY last entry is being appended
+            // card.addEventListener('click', flipCard); // not created yet
             grid.appendChild(card);
             console.log(card);   // testing
         }
